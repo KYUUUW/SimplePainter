6d42e7bffefdbf9c482a21eb3bd261a66b474941
KYUUUW <duffufk1202@naver.com>
2016-12-11 18:07:35 +0900

    주석안단것 나머지 다 주석 담.

--- src/DrawConstants.java
+++ src/DrawConstants.java
@@ -1,11 +1,11 @@
 public class DrawConstants
 {
-	static public final String[] MENU = {"DOT","LINE","RECT","OVAL","",""};
+	static public final String[] MENU = {"DOT","LINE","RECT","OVAL","",""};	//	메뉴 글씨 써주기 위해서
 	
-	static public final int DOT		= 0;
+	static public final int DOT		= 0;	//	파라미터 이름 지정...
 	static public final int LINE	= 1;
 	static public final int RECT	= 2;
 	static public final int OVAL	= 3;
 	
-	static public final int NONE	= 6;
+	static public final int NONE	= 6;	// 6개 버튼이므로 5번 까지 있음. 6번이면 default임.
 } // DrawConstants class
